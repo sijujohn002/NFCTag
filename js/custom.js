6c125622b156477a30d44e832cf33583471790f4
sijujohn002 <vijandasan001@gmail.com>
2019-12-04 16:06:26 +0530

    updated New flow

--- js/custom.js
+++ js/custom.js
@@ -4,98 +4,131 @@
 		  
 		  
 		  
-  $('.add-card').click(function(){
-            $('.add-card-details').show();
-            $('.spn').removeClass('active');
-            });
-
-            $('.master-card').click(function(){
-                $('.spn').addClass('active');
-                $('.add-card-details').hide();
-                });
-
-
-        $('#payment-success').click(function(){
-        $('#without-amount').hide();
-       $('#with-amount').hide();
-       $('#payment-completed').show();
-
-});
-
-
-$('#back-page').click(function(){
-       $('#without-amount').hide();
-       $('#with-amount').show();
-       $('#payment-completed').hide();
-});
-
-
-
-$('#amount-added').click(function(){
-       $('#without-amount').hide();
-       $('#with-amount').hide();
-       $('#payment-completed').show();
-       
-       var amount = $('#amount').val();
-          $('.total-price').text("$ "+amount); 
-   
-});
-     
-var unit=1
-        $('.qty').click(function(){
+        $('.add-card').click(function(){
+                  $('.add-card-details').show();
+                  $('.spn').removeClass('active');
+                  });
+      
+                  $('.master-card').click(function(){
+                      $('.spn').addClass('active');
+                      $('.add-card-details').hide();
+                      });
+      
+      
+              $('#payment-success').click(function(){
+              $('#without-amount').hide();
+             $('#with-amount').hide();
+             $('#payment-completed').show();
+      
+      });
+      
+      
+      $('#back-page').click(function(){
+             $('#without-amount').hide();
+             $('#with-amount').show();
+             $('#payment-completed').hide();
+      });
+      
+      
+      
+      $('#amount-added').click(function(){
+             $('#without-amount').hide();
+             $('#with-amount').hide();
+             $('#payment-completed').show();
+             
+             var extrawithoutprice = $('#extrawithoutprice').val();
+             var amount = $('#amount').val();
+             $('.total-price').text("$ "+(parseInt(amount)+parseInt(extrawithoutprice))); 
          
-           calculatePrice($(this).text());
       });
-      function calculatePrice(Text){
-          if(Text === '+')
-          unit++
-          else
-          if(unit>1)
-          unit--
-          $('#unit').text(unit);
-          $('.total-price').text("$ " +unit*priceInt); 
-
-          
- 
-      }
- var locationUrl = window.location.href
- function PareseUrl(item, text){
-     var foundString = text.match(new RegExp("[\?\&]" + item + "=([^\&]*)(\&?)","i"));
-     return foundString ? foundString[1] : foundString;
- }
- var data=PareseUrl('info', locationUrl);
- var info = atob(data);
- console.log("info data:"+info);
- var isPrice=false
- if(info){
-     var jsondata=JSON.parse(info)
-     if(jsondata.MTA){
-     var priceInt = parseInt(jsondata.MTA)
-     if(priceInt>0)
-     isPrice=true
-   }  
- } 
+           
+      $("#extrawithprice").bind('input', function() {
+        var extrawithprice = $('#extrawithprice').val();
+        $('.total-price').text("$ " +((parseInt(unit)*parseInt(priceInt))+parseInt(extrawithprice)));
+        });
+      
+        $("#extrawithoutprice").bind('input', function() {
+          var extrawithoutprice = $('#extrawithoutprice').val();
+          var amount = $('#amount').val();
+          $('.total-price-not').text("$ "+(parseInt(amount)+parseInt(extrawithoutprice))); 
+          });
+          $("#amount").bind('input', function() {
+            var extrawithoutprice = $('#extrawithoutprice').val();
+            var amount = $('#amount').val();
+            if(parseInt(extrawithoutprice)>0)
+            $('.total-price-not').text("$ "+(parseInt(amount)+parseInt(extrawithoutprice))); 
+            else
+            $('.total-price-not').text("$ "+(parseInt(amount))); 
+            });
+      
+      var unit=1
+              $('.qty').click(function(){
+               
+                 calculatePrice($(this).text());
+            });
+            function calculatePrice(Text){
+                if(Text === '+')
+                unit++
+                else
+                if(unit>1)
+                unit--
+                $('#unit').text(unit);
+                var extrawithprice = $('#extrawithprice').val();
+                $('.value-price').text("$ " +(unit*priceInt)); 
+       var extrawithprice = $('#extrawithprice').val();
+       if(parseInt(extrawithprice)>0)
+        $('.total-price').text("$ " +((parseInt(unit)*parseInt(priceInt))+parseInt(extrawithprice)));
+        else{
+          $('.total-price').text("$ " +((parseInt(unit)*parseInt(priceInt))));
+        }
+                
        
- if(!isPrice){
-  $('#with-amount').hide();
-  $('#without-amount').show();
-  
-  }else{
-    $('#with-amount').show();
-    $('#without-amount').hide();
-    $('.total-price').text("$ "+priceInt); 
-    $('#singleItemPrice').text("$"+priceInt);
-  } 
+            }
+            
+       var locationUrl = window.location.href
+       function PareseUrl(item, text){
+           var foundString = text.match(new RegExp("[\?\&]" + item + "=([^\&]*)(\&?)","i"));
+           return foundString ? foundString[1] : foundString;
+       }
+       var data=PareseUrl('info', locationUrl);
+       var info = atob(data);
+       console.log("info data:"+info);
+       var isPrice=false
+       if(info){
+           var jsondata=JSON.parse(info)
+           if(jsondata.MTA){
+           var priceInt = parseInt(jsondata.MTA)
+           if(priceInt>0)
+           isPrice=true
+         }  
+       } 
+       
+      
+       
+      
+       
+      
+       if(!isPrice){
+        $('#with-amount').hide();
+        $('#without-amount').show();
         
-
-
-        $('.add-remove').slick({
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  arrows: false
-});
-
-
+        }else{
+          $('#with-amount').show();
+          $('#without-amount').hide();
+          $('.value-price').text("$ "+priceInt); 
+          $('#singleItemPrice').text("$"+priceInt);
+          $('.total-price').text("$ " +(priceInt))
+        } 
+              
+      
+      
+              $('.add-remove').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false
       });
-  
-    
+      
+      
+            });
+        
+          
